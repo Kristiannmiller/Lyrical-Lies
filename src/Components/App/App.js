a4a7dc06733478558b45c7c2c1f765403a247e72
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-07 10:09:00 -0700

    Change App to class component to add state and displayLyrics method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,58 +1,76 @@
 import './App.css';
-import React from 'react';
+import {Component} from 'react';
 import {BrowserRouter, Route, Switch, NavLink} from 'react-router-dom';
 import logo from '../../Assets/lyricalLiesLogo.png'
 import faves from '../../Assets/favesIcon.png'
 import home from '../../Assets/homeIcon.png'
 import LandingPage from '../LandingPage/LandingPage.js'
 import HomePage from '../HomePage/HomePage.js'
+import LyricsPage from '../LyricsPage/LyricsPage.js'
+import { getLyrics } from '../../apiCalls.js'
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <main>
-        <header>
-          <img
+class App extends Component {
+  constructor() {
+    super()
+    this.state= {
+      songInfo: {},
+      lyrics: ""
+    }
+  }
+  displayLyrics = async (songInfo) => {
+    const foundLyrics = await getLyrics(songInfo.artist.name, songInfo.title)
+    this.setState({songInfo: songInfo, lyrics: foundLyrics.lyrics})
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <main>
+          <header>
+            <img
             className="logo"
             title="Lyrical Lies Logo"
             src={logo}
             alt="Lyrical Lies Logo"
-          />
-          <section className="navigationWrap">
-          <Route>
-            <NavLink id="homeButton" activeClassName="activeHomeButton" to='/home'>
-              <img
-                className="navImg"
-                title="Return to Homepage"
-                src={home}
-                alt="Navigate back to home page"
-              />
-            </NavLink>
+            />
+            <section className="navigationWrap">
+              <Route>
+                <NavLink id="homeButton" activeClassName="activeHomeButton" to='/home'>
+                  <img
+                  className="navImg"
+                  title="Return to Homepage"
+                  src={home}
+                  alt="Navigate back to home page"
+                  />
+                </NavLink>
+              </Route>
+              <Route>
+                <NavLink id="favesButton" activeClassName="activeFavesButton" to='/faves'>
+                  <img
+                  className="navImg"
+                  title="View Favorite Lyrics"
+                  src={faves}
+                  alt="Navigate to favorites page"
+                  />
+                </NavLink>
+              </Route>
+            </section>
+          </header>
+        <Switch>
+          <Route path='/home'>
+            <HomePage displayLyrics={this.displayLyrics}/>
           </Route>
-          <Route>
-            <NavLink id="favesButton" activeClassName="activeFavesButton" to='/faves'>
-              <img
-                className="navImg"
-                title="View Favorite Lyrics"
-                src={faves}
-                alt="Navigate to favorites page"
-              />
-            </NavLink>
+          <Route path='/lyrics'>
+            <LyricsPage lyrics={this.state.lyrics}/>
           </Route>
-          </section>
-        </header>
-      <Switch>
-        <Route path='/home'>
-          <HomePage />
-        </Route>
-        <Route path='/lyrics'></Route>
-        <Route exactPath='/'>
-          <LandingPage />
-        </Route>
-      </Switch>
-      </main>
-    </BrowserRouter>
-  );
+          <Route exactPath='/'>
+            <LandingPage />
+          </Route>
+        </Switch>
+        </main>
+      </BrowserRouter>
+    );
+
+  }
 }
 
 export default App;
