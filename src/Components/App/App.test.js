dfcd6c122a7ea295ad159a812349b22be2f7193c
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-06 20:28:32 -0700

    Add testing pseudocode

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -4,3 +4,9 @@ import App from './App';
 test('works', () => {
   expect(true).toBe(true);
 });
+
+
+// TESTING IDEAS ::
+
+// It should render a header
+  //expect that alt text is in document for each
