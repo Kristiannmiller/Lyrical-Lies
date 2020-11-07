1886952b3e41499aaa074279ea0b1b35907cdacb
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-06 22:52:30 -0700

    Add testing pseudocode

--- src/Components/HomePage/HomePage.test.js
+++ src/Components/HomePage/HomePage.test.js
@@ -10,10 +10,6 @@ test('works', () => {
 // It should render a form
   //expect that form placeholder text is in document
 
-// It should display up to 5 search results
-  //mock api call with more than 5 results
-  //type in 'Elton'
-  //expect that only 5 results are in document
 
 // It should render the result list after each change to the input
   //mock api call with two results
