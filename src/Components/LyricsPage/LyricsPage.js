6be85b786288b56646bd528a3c551b71cd698e72
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-07 14:17:54 -0700

    Add conditional rendering for loading and error handling

--- src/Components/LyricsPage/LyricsPage.js
+++ src/Components/LyricsPage/LyricsPage.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import logo from '../../Assets/lyricalLiesLogo.png'
 import './LyricsPage.css'
 
-const LyricsPage = ({songInfo, lyrics}) => {
-  console.log(songInfo, lyrics);
+const LyricsPage = ({songInfo, lyrics, error}) => {
+  console.log(songInfo, lyrics, "and error", error);
+  if(error || !lyrics) {
+    return (
+      <section className="landingWrap">
+        <img
+          className="welcomeLogo"
+          title="Welcome to Lyrical Lies"
+          src={logo}
+          alt="welcome to Lyrical Lies"
+        />
+        <h2 className="message">
+        {error ? error : `Putting that record on now`}</h2>
+      </section>
+    )
+  }
   return (
     <section className="lyricsWrap">
       <section className="lyrics">
