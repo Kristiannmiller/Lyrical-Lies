7da2f653e88be0b6e0231ae65ea61e1063288d49
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-06 19:45:56 -0700

    Add header

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,46 @@
 import './App.css';
-
+import {BrowserRouter, Route, Switch, NavLink} from 'react-router-dom';
+import logo from '../Assets/lyricalLiesLogo.png'
+import faves from '../Assets/favesIcon.png'
+import home from '../Assets/homeIcon.png'
 function App() {
   return (
-    <div className="App">
-      <h1>Lyrical Lies</h1>
-    </div>
+    <BrowserRouter>
+      <main>
+        <header>
+          <img
+            className="logo"
+            title="Return To Lobby"
+            src={logo}
+            alt="Navigate back to home page"
+          />
+          <section className="navigationWrap">
+          <Route>
+            <NavLink id="homeButton" activeClassName="activeHomeButton" to='/home'>
+              <img
+                className="navImg"
+                title="Return To Lobby"
+                src={home}
+                alt="Navigate back to home page"
+              />
+            </NavLink>
+          </Route>
+          <Route>
+            <NavLink id="favesButton" activeClassName="activeHomeButton" to='/faves'>
+              <img
+                className="navImg"
+                title="Return To Lobby"
+                src={faves}
+                alt="Navigate back to home page"
+              />
+            </NavLink>
+          </Route>
+          </section>
+        </header>
+      <Switch>
+      </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
