fe4fa7fe32b247bc17f73d8f9e5944e51423d3aa
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-07 10:10:23 -0700

    Add handleClick method to displayLyrics

--- src/Components/ResultList/ResultList.js
+++ src/Components/ResultList/ResultList.js
@@ -1,6 +1,7 @@
 import {Component} from 'react';
-import './ResultList.css'
-import { getSuggestions } from '../../apiCalls.js'
+import './ResultList.css';
+import { getSuggestions } from '../../apiCalls.js';
+import {NavLink} from 'react-router-dom';
 
 class ResultList extends Component {
   constructor(props) {
@@ -18,6 +19,11 @@ class ResultList extends Component {
       this.setState({results: searchResults.data})
     }
   }
+  handleClick = (event) => {
+    const songInfo = this.state.results.find(result => {
+      return result.id === +event.target.parentElement.id})
+    this.props.displayLyrics(songInfo)
+  }
   componentDidUpdate(prevProps) {
     if(this.props.input !== prevProps.input) {
       this.displayResults(this.props.input)
@@ -30,10 +36,10 @@ class ResultList extends Component {
     const resultData = this.state.results.map((result, i) => {
       if( i < 6 ) {
         return (
-        <button>
+        <NavLink to="/lyrics" onClick={this.handleClick} id={result.id} className="resultCard">
           <h1>{result.artist.name}</h1>
           <h2>{result.title}</h2>
-        </button>
+        </NavLink>
       )}
     })
     return (
