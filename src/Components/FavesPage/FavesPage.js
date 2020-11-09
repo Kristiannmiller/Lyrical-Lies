95ee2f9ab92d18a58dd9a45ec30a4a24219b66d3
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-09 16:59:17 -0700

    Add functionality for displaying all favorited comments

--- src/Components/FavesPage/FavesPage.js
+++ src/Components/FavesPage/FavesPage.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import './FavesPage.css'
+import faveIcon from '../../Assets/FaveIcon.svg'
+import faveActive from '../../Assets/FaveActive.svg'
+import logo from '../../Assets/lyricalLiesLogo.png'
 import PropTypes from 'prop-types'
 
 
-const FavesPage = ({ comments }) => {
+const FavesPage = ({ comments, updateComment }) => {
+  let cards = []
+  let faveComments
+  if (comments.length) {
+    faveComments = comments.filter(comment => comment.fave === true)
+  }
+  if (!comments.length || !faveComments.length) {
+    cards.push(
+      <section>
+        <img
+          className="welcomeLogo"
+          title="Welcome to Lyrical Lies"
+          src={logo}
+          alt="welcome to Lyrical Lies"
+        />
+        <h2 className="message">You have no favorite lyrical lies! Go find some!</h2>
+      </section>
+    )
+  } else {
+    cards = faveComments.map((comment, index) => {
+      return (
+        <section key={index} className="faveCard">
+          <img
+            id={comment.id}
+            onClick={updateComment}
+            className="faveIcon"
+            src={comment.fave ? faveActive : faveIcon}
+            alt={comment.fave ? "active favorite icon" : "inactive favorite icon"}
+          />
+          <h1 className="faveTitle">{comment.songTitle}</h1>
+          <h2 className="faveArtist">{comment.artist}</h2>
+          <h3 className="faveComment">"{comment.comment}"</h3>
+        </section>
+      )
+    })
+  }
   return (
     <section data-testid="favesWrap" className="favesWrap">
-      <h2>Your favorite lyrical lies</h2>
+      <h1 className="faveHeader">{faveComments && 'Your favorite lyrical lies'}</h1>
+      <section className="cardContainer">
+        {cards}
+      </section>
     </section>
   )
 }
